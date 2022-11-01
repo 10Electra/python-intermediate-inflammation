c0706b5ba8cad8465709f0a7d5717e6229c84ed8
10Electra <63862340+10Electra@users.noreply.github.com>
2022-11-01 12:53:03 +0000

    Improve style using linting and update requirements

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
@@ -21,7 +21,7 @@ def load_csv(filename):
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
-    
+
     :param data: 2D Numpy array containing inflammation data
     :returns: A one-dimensional Numpy array containing mean averages"""
     return np.mean(data, axis=0)
@@ -29,7 +29,7 @@ def daily_mean(data):
 
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
-    
+
     :param data: 2D Numpy array containing inflammation data
     :returns: A one-dimensional Numpy array containing the max value of each column"""
     return np.max(data, axis=0)
@@ -37,8 +37,7 @@ def daily_max(data):
 
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
-    
+
     :param data: 2D Numpy array containing inflammation data
     :returns: A one-dimensional Numpy array containing the min value of each column"""
     return np.min(data, axis=0)
-
