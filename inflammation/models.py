56be71c67f05667532496cc99c9482d429edad2d
10Electra <63862340+10Electra@users.noreply.github.com>
2022-11-07 13:00:43 +0000

    add patient class

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,6 +10,29 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
+class Patient:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.observations = []
+    
+    def add_observation(self,value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1]['day'] + 1
+            except IndexError:
+                day = 0
+        
+        new_observation = {
+            'day': day,
+            'value': value
+        }
+        self.observations.append(new_observation)
+        return new_observation
+    
+    def __str__(self) -> str:
+        return self.name
+
+
 def load_csv(filename):
     """Load a Numpy array from a CSV
 
