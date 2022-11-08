6a8b7772ea7bef4d4658263133a768305e54575a
10Electra <63862340+10Electra@users.noreply.github.com>
2022-11-08 10:37:26 +0000

    Add new test

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -6,9 +6,17 @@ def test_create_patient():
 
     name = 'Alice'
     p = Patient(name=name)
+
+    assert p.name == name
+
+def test_patient_observations():
+    from inflammation.models import Patient
+    
+    name = 'Alice'
+    p = Patient(name=name)
+
     p.add_observation('super strong today', day=0)
     p.add_observation('looking a little green')
 
-    assert p.name == name
     assert [observation.value for observation in p.observations] == ['super strong today', 'looking a little green']
     assert [observation.day for observation in p.observations] == [0,1]
